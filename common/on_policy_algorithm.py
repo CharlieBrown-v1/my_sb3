157709f147ed79e940e35f35bed3f0d1d775a117
Stalin <2834715238@qq.com>
2022-04-17 21:00:03 +0800

    update is_successes recording

--- common/on_policy_algorithm.py
+++ common/on_policy_algorithm.py
@@ -269,7 +269,7 @@ class OnPolicyAlgorithm(BaseAlgorithm):
                     self.logger.record("rollout/ep_len_mean",
                                        safe_mean([ep_info["l"] for ep_info in self.ep_info_buffer]))
                     if len(self.ep_success_buffer) > 0:
-                        self.logger.record("rollout/is_success", safe_mean(self.ep_success_buffer))
+                        self.logger.record("rollout/success_rate", safe_mean(self.ep_success_buffer))
                 self.logger.record("time/fps", fps)
                 self.logger.record("time/time_elapsed", int(time.time() - self.start_time), exclude="tensorboard")
                 self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
